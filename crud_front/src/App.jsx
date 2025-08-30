098497150708fcada5b98b4ec1cf3c348704476d
yarasulakshmikala <143312838+yarasulakshmikala@users.noreply.github.com>
2025-08-30 16:18:38 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:9090/springapp1';
+const BASE_URL = 'http://13.204.51.234:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
